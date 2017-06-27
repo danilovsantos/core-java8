d52e5e893520d911e10aea1fc738caa8356f1771
Danilo Valente Dos Santos <dvsantos1@stefanini.com>
2017-06-27 18:01:52 -0300

    Functional Programming - Function Sample

--- src/main/java/com/java/functional/programing/FunctionSample.java
+++ src/main/java/com/java/functional/programing/FunctionSample.java
@@ -22,6 +22,7 @@ public class FunctionSample {
 	 * and the parameter vacation is true if we are on vacation. 
 	 * We sleep in if it is not a weekday or we're on vacation. 
 	 * Return true if we sleep in.  
+	 * http://codingbat.com/prob/p187868
 	 */
 	
 	public boolean sleepIn(boolean weekday, boolean vacation){
