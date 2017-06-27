923678ccb7eae1d6331667fb30ddd089b302e152
Danilo Valente Dos Santos <dvsantos1@stefanini.com>
2017-06-27 18:00:37 -0300

    Functional Programming - Function Sample

--- src/main/java/com/java/functional/programing/FunctionSample.java
+++ src/main/java/com/java/functional/programing/FunctionSample.java
@@ -1,11 +1,48 @@
 package com.java.functional.programing;
 
+import java.util.function.Function;
+
 public class FunctionSample {
 
 	public static void main(String[] args) {
 		
+		FunctionSample f = new FunctionSample();
+		
+		System.out.println(f.sleepIn(true, true));
+		System.out.println(f.sleepIn(false, false));
+		System.out.println(f.sleepIn(true, false));
+		System.out.println(f.sleepIn(false, true));
 		
 
+	}
+	
+	
+	/**
+	 * The parameter weekday is true if it is a weekday, 
+	 * and the parameter vacation is true if we are on vacation. 
+	 * We sleep in if it is not a weekday or we're on vacation. 
+	 * Return true if we sleep in.  
+	 */
+	
+	public boolean sleepIn(boolean weekday, boolean vacation){
+		
+		Function<Boolean, Boolean> isWeekDay = new Function<Boolean, Boolean>(){
+			public Boolean apply(Boolean b) {
+				return b;
+			}
+		};
+		
+		Function<Boolean, Boolean> isVacationDay = new Function<Boolean, Boolean>(){
+			public Boolean apply(Boolean b) {
+				return b;
+			}
+		};
+		
+		if(isWeekDay.apply(weekday) && !isVacationDay.apply(vacation)){
+			return false;
+		}
+		
+		return true;
 	}
 
 }
---
+++ src/main/java/com/java/functional/programing/ProviderConsumerSample.java
@@ -0,0 +1,9 @@
+package com.java.functional.programing;
+
+public class ProviderConsumerSample {
+
+	public static void main(String[] args) {		
+		
+	}
+
+}
