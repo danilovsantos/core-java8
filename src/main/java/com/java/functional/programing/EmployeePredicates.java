1e0e0604b5998aab65265daf80e9945a58ca22cb
Danilo Valente Dos Santos <dvsantos1@stefanini.com>
2017-06-29 11:25:54 -0300

    Functional Programming - More Predicates

--- src/main/java/com/java/functional/programing/EmployeePredicates.java
+++ src/main/java/com/java/functional/programing/EmployeePredicates.java
@@ -1,5 +1,6 @@
 package com.java.functional.programing;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -8,20 +9,78 @@ import java.util.stream.Collectors;
 
 public class EmployeePredicates {
 
-	public static Predicate<Employee> isAdult(){
+	private Integer id;
+	private Integer age;
+	private String name;
+	
+	public EmployeePredicates(){}
+	
+	public EmployeePredicates(Integer id, Integer age, String name){
+		this.id = id;
+		this.age = age;
+		this.name = name;
+	}
+	
+	public static void main(String[]args){
+		
+		List<EmployeePredicates> employees = new ArrayList<EmployeePredicates>();
+		
+		employees.add(new EmployeePredicates(1, 30, "Fulano"));
+		employees.add(new EmployeePredicates(2, 17, "Ciclano"));
+		employees.add(new EmployeePredicates(3, 15, "Zé"));
+		employees.add(new EmployeePredicates(4, 20, "João"));
+		employees.add(new EmployeePredicates(5, 50, "Tônio"));
+		
+		System.out.println(EmployeePredicates.filterEmployees(employees, isAdult()).toString());
+		System.out.println(EmployeePredicates.filterEmployees(employees, checkById(3)).toString());
+		System.out.println(EmployeePredicates.filterEmployees(employees, checkByName("João")).toString());
+		
+	}
+	
+	public static Predicate<EmployeePredicates> isAdult(){
 		return e -> e.getAge() > 18;
 	}
 	
-	public static Predicate<Employee> checkById(Integer id){
+	public static Predicate<EmployeePredicates> checkById(Integer id){
 		return e -> e.getId() == id;
 	}
 	
-	public static Predicate<Employee> checkByName(String name){
-		return e -> e.equals(name);
+	public static Predicate<EmployeePredicates> checkByName(String name){
+		return e -> e.getName().equals(name);
 	}
 	
-	public static List<Employee> filterEmployees(List<Employee> employees, Predicate<Employee> predicate){
-		return employees.stream().filter(predicate).collect(Collectors.<Employee>toList());
+	public static List<EmployeePredicates> filterEmployees(List<EmployeePredicates> employees, Predicate<EmployeePredicates> predicate){
+		return employees.stream().filter(predicate).collect(Collectors.<EmployeePredicates>toList());
 	}
+		
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return this.id+" - "+this.name+" - "+this.age;
+	}
+
 	
 }
