3c7dbc7a1f52632043f09def5c362c0f69a0be18
Danilo Valente Dos Santos <dvsantos1@stefanini.com>
2017-07-03 15:28:52 -0300

    More Codding

---
+++ src/main/java/com/java/functional/programing/ReadFileTestJava7.java
@@ -0,0 +1,44 @@
+package com.java.functional.programing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class ReadFileTestJava7 {
+	
+	public static void main(String[]args){
+		
+		try {
+			
+			System.out.println(Calendar.getInstance().getTimeInMillis());
+			System.out.println(Runtime.getRuntime().totalMemory());
+			
+			Path path = FileSystems.getDefault().getPath("C:/Users/dvsantos1/Desktop/mockdata.csv");
+			InputStream is = Files.newInputStream(path);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			List<String> lines = new ArrayList<String>();
+			
+			String line = null;
+			
+			while((line = br.readLine()) != null){
+				lines.add(line);
+			}
+			
+			System.out.println(Calendar.getInstance().getTimeInMillis());
+			System.out.println(Runtime.getRuntime().totalMemory());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ src/main/java/com/java/functional/programing/WeakReferenceTest.java
@@ -0,0 +1,10 @@
+package com.java.functional.programing;
+
+public class WeakReferenceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
