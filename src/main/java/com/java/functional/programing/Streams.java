59216a159711347e88edf576e4b0a1252775b594
Danilo Valente Dos Santos <dvsantos1@stefanini.com>
2017-07-04 15:46:14 -0300

    More Codding

--- src/main/java/com/java/functional/programing/Streams.java
+++ src/main/java/com/java/functional/programing/Streams.java
@@ -2,11 +2,20 @@ package com.java.functional.programing;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Streams {
 
 	public static void main(String[] args) {
+		
+		//listToStream();
+		//streamOfStrings();
+		//streamOneToTen();
+
+	}
+	
+	public static void listToStream(){
 		
 		List<Integer> numbers = new ArrayList<Integer>();
 		
@@ -20,7 +29,19 @@ public class Streams {
 		
 		//stream.filter(t -> t > 2).forEach(i -> System.out.println(i));
 		stream.sorted((first,second) -> second - first).forEach(System.out::println);
-
+		
+	}
+	
+	
+	public static void streamOfStrings(){
+		Stream<String> strings = Stream.of("Cofee", "Code", "Java");
+		strings.forEach(System.out::println);
+	}
+	
+	
+	public static void streamOneToTen(){
+		IntStream it = IntStream.rangeClosed(0, 10).skip(1);
+		it.forEach(i -> System.out.println(i));
 	}
 
 }
